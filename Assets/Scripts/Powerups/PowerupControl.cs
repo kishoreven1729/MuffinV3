260b3b825b0093eb23a96f6696da7c45e06035b7
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-26 17:34:40 -0700

    PowerupControl updated

---
+++ Assets/Scripts/Powerups/PowerupControl.cs
@@ -0,0 +1,68 @@
+﻿#region References
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public class PowerupControl : MonoBehaviour 
+{
+	#region Private Variables
+	private float		_powerupExpiryInterval;
+	#endregion
+
+	#region Public Variables
+	public PowerupManager.PowerupType 	powerupType;
+	#endregion
+
+	#region Constructor
+	void Awake()
+	{
+		_powerupExpiryInterval = 5.0f;
+
+		StartCoroutine("Expiry");
+	}
+
+	void Start() 
+	{
+	}
+	#endregion
+	
+	#region Loop
+	void Update() 
+	{
+
+	}
+
+	public void OnTriggerEnter(Collider otherCollider)
+	{
+		if(otherCollider.CompareTag("Player"))
+		{
+			PowerupManager.powerupManagerInstance.availablePowerupType = powerupType;
+
+			StopCoroutine("Expiry");
+
+			RemovePowerup();
+		}
+	}
+	#endregion
+
+	#region Methods
+	public IEnumerator Expiry()
+	{
+		yield return new WaitForSeconds(_powerupExpiryInterval);
+
+		RemovePowerup();
+	}
+
+	public void RemovePowerup()
+	{
+		PowerupManager.powerupManagerInstance.RemovePowerup(transform.name);
+
+		Destroy(transform.gameObject);
+	}
+
+	public void AssignPowerup(PowerupManager.PowerupType randomPowerupType)
+	{
+		powerupType = randomPowerupType;
+	}
+	#endregion
+}
